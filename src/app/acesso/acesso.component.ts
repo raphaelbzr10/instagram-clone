501e54a20ac018dbe626fd56e2bfa7678b146c72
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-07 15:24:36 -0300

    customizando a animação do painel login e cadastro com keyframes

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -20,8 +20,21 @@ import { Component, OnInit } from '@angular/core';
         'opacity': 1
       })),
       transition('void => criado', [
-        style({'opacity': 0, 'transform': 'translate(50px, 0)'}),
-        animate('500ms 0s ease-in-out')
+        style({opacity: 0, transform: 'translate(50px, 0'}),
+        //0 void -------X----------------X--Y--Y----Y 1.5s//
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.86, opacity: 1, transform: 'translateX(0)'}),
+
+          style({offset: 0.88, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.90, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 0.92, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.94, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 0.96, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.98, opacity: 1, transform: 'translateY(10px)'}),
+          
+          style({offset: 1, opacity: 1, transform: 'translateY(0)'})
+        ])) //duração, delay e aceleração
       ])
     ])
   ]
