9cb750ace855044984ff7dd50d1b4fa8c0700889
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-07 11:03:36 -0300

    renderização condicional do login e do cadastro

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -31,6 +31,8 @@ export class AcessoComponent implements OnInit {
   public estadoBanner: string = 'criado';
   public estadoPainel: string = 'criado';
 
+  public cadastro: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
