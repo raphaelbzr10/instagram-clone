618b00ac5f6d530900ad151788d1564ab050b895
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-08 11:14:34 -0300

    controlando o formulário de cadastro

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,6 +10,13 @@ export class CadastroComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>();
 
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'nome_completo': new FormControl(null),
+    'nome_usuario': new FormControl(null),
+    'senha': new FormControl(null),
+  })
+
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +24,10 @@ export class CadastroComponent implements OnInit {
 
   public exibirPainelLogin(): void {
     this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario(): void {
+    console.log(this.formulario);
   }
 
 }
