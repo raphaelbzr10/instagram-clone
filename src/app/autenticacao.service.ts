6d54b142b300990508d9e89a75c987b489c848e7
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-08 17:55:14 -0300

    cadastrando usuários no firebase - parte 2

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -3,11 +3,15 @@ import * as firebase from 'firebase';
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
-        console.log('chegamos até o serviço: ', usuario);
 
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
-                console.log(resposta);
+                // remover a senha do usuário do objeto
+                delete usuario.senha;
+
+                // registrando dados complementares do usuário no path email na base64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    .set(usuario);
             })
             .catch((error: Error) => {
                 console.log(error);
