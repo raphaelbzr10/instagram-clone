01248640cd8e37e9e38b5c7571857e2f997a4aab
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-08 14:37:01 -0300

    conectando o firebase sdk do projeto com o backend do firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    let firebaseConfig = {
+      apiKey: "AIzaSyDL8_b0CV7BQkn4xzljEkOzJvuXeMIj2oQ",
+      authDomain: "jta-instagram-clone-de9ee.firebaseapp.com",
+      projectId: "jta-instagram-clone-de9ee",
+      storageBucket: "jta-instagram-clone-de9ee.appspot.com",
+      messagingSenderId: "875551680230",
+      appId: "1:875551680230:web:10bf511171671c417c3d27",
+      measurementId: "G-JG27CH75XT"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
