ef75f5b674b34ea3ffaab0b6021227b73e7dd93b
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-09 19:15:36 -0300

    efetuando logout

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -49,4 +49,13 @@ export class Autenticacao {
 
         return this.token_id !== undefined;
     }
+
+    public sair(): void {
+        firebase.auth().signOut()
+            .then(() => {
+                localStorage.removeItem('idToken');
+                this.token_id = undefined;
+                this.router.navigate(['/']);
+            });
+    }
 }
