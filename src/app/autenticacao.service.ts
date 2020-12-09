b8a088d1377adffbc34d0837b5e9422e0130f734
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-09 19:19:02 -0300

    redirecionando para o path raiz se o usuário não estiver autenticado

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -47,6 +47,10 @@ export class Autenticacao {
             this.token_id = localStorage.getItem('idToken');
         }
 
+        if (this.token_id === undefined) {
+            this.router.navigate(['/']);
+        }
+
         return this.token_id !== undefined;
     }
 
