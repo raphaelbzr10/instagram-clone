e4516f27a347925c72b81f5dc3eed950555f0d30
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-09 19:05:45 -0300

    utilizando localStorage para armazenamento do token de autenticação

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -32,6 +32,7 @@ export class Autenticacao {
                 firebase.auth().currentUser.getIdToken()
                     .then((idToken: string) => {
                         this.token_id = idToken;
+                        localStorage.setItem('idToken', idToken);
                         this.router.navigate(['/home']);
                     })
             })
@@ -41,6 +42,11 @@ export class Autenticacao {
     }
 
     public autenticado(): boolean {
+
+        if (this.token_id === undefined && localStorage.getItem('idToken') != null) {
+            this.token_id = localStorage.getItem('idToken');
+        }
+
         return this.token_id !== undefined;
     }
 }
