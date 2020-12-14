22b3aeb123485131820a4b50bdb82d579e96e580
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-14 10:57:21 -0300

    controlando o progresso do upload

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -9,6 +9,18 @@ export class Bd {
         let nomeImagem = Date.now();
 
         firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`).put(publicacao.imagem);
+            .child(`imagens/${nomeImagem}`)
+            .put(publicacao.imagem)
+            .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                (snapshot: any) => {
+                    console.log(snapshot);
+                },
+                (error) => {
+                    console.log(error);
+                },
+                () => {
+                    console.log('upload completo');
+                }
+            )
     }
 }
