3c7bc74dc37f6ef2ff38b378af6d1d26b9d38a4b
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-15 11:18:26 -0300

    atualizando a timeline após inclusao de nova publicacao - ViewChild

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Autenticacao } from '../autenticacao.service';
 
 @Component({
@@ -7,6 +7,8 @@ import { Autenticacao } from '../autenticacao.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild('publicacoes') public publicacoes: any;
 
   constructor(private autenticacao: Autenticacao) { }
 
@@ -18,7 +20,7 @@ export class HomeComponent implements OnInit {
   }
 
   public atualizarTimeLine(): void {
-    console.log('chegamos até aqui');
+    this.publicacoes.atualizarTimeLine();
   }
 
 }
